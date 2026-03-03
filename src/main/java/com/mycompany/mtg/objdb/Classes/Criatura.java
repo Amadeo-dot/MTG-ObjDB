6e108aa2fc15f8f82bb7a2ca70116604c726ea8c
Alumne Matí <alumne@localhost.localdomain>
2026-03-03 11:38:06 +0100

    Reparaciones

---
+++ src/main/java/com/mycompany/mtg/objdb/Classes/Criatura.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mtg.objdb.Classes;
+
+/**
+ *
+ * @author alumne
+ */
+public class Criatura {
+    
+    int forca;
+    int resistencia;
+    String tipusCriatura;
+    boolean vola;
+}
